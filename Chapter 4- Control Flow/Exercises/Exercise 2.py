7c0fff3b94378595a02fa2b17731cb1cc57bcb82
DietherDuario <DuarioDiether@gmail.com>
2025-01-03 02:29:28 +0400

    Chapter 4

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+alien_color = "Red"
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,12 @@
+favorites = ["Peach", "Apple", "Orange"]
+
+if "Peach" in favorites:
+    print("You really like peaches!")
+if "Apple" in favorites:
+    print("You really like apples!")
+if "Orange" in favorites:
+    print("You really like oranges!")
+if "Olives" in favorites:
+    print("You really like olives!")
+if "Watermelon" in favorites:
+    print("You really like watermelons!")
